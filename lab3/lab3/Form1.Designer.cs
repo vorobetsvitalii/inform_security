c0a26dcf6de1fbe7615c2f6009ae6ea7059051e7
vorobetsvitalii <vorobetsvitaliy3@gmail.com>
2024-09-10 17:33:56 +0300

    lab3

---
+++ lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btnLoadTable = new System.Windows.Forms.Button();
+            this.btnDecryptText = new System.Windows.Forms.Button();
+            this.txtDecryptedText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+
+            // 
+            // btnLoadTable
+            // 
+            this.btnLoadTable.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadTable.Name = "btnLoadTable";
+            this.btnLoadTable.Size = new System.Drawing.Size(200, 30);
+            this.btnLoadTable.TabIndex = 0;
+            this.btnLoadTable.Text = "Завантажити таблицю";
+            this.btnLoadTable.UseVisualStyleBackColor = true;
+            this.btnLoadTable.Click += new System.EventHandler(this.btnLoadTable_Click);
+
+            // 
+            // btnDecryptText
+            // 
+            this.btnDecryptText.Location = new System.Drawing.Point(12, 48);
+            this.btnDecryptText.Name = "btnDecryptText";
+            this.btnDecryptText.Size = new System.Drawing.Size(200, 30);
+            this.btnDecryptText.TabIndex = 1;
+            this.btnDecryptText.Text = "Розшифрувати текст";
+            this.btnDecryptText.UseVisualStyleBackColor = true;
+            this.btnDecryptText.Click += new System.EventHandler(this.btnDecryptText_Click);
+
+            // 
+            // txtDecryptedText
+            // 
+            this.txtDecryptedText.Location = new System.Drawing.Point(12, 84);
+            this.txtDecryptedText.Multiline = true;
+            this.txtDecryptedText.Name = "txtDecryptedText";
+            this.txtDecryptedText.Size = new System.Drawing.Size(480, 200);
+            this.txtDecryptedText.TabIndex = 2;
+
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Controls.Add(this.txtDecryptedText);
+            this.Controls.Add(this.btnDecryptText);
+            this.Controls.Add(this.btnLoadTable);
+            this.Name = "MainForm";
+            this.Text = "Дешифратор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadTable;
+        private System.Windows.Forms.Button btnDecryptText;
+        private System.Windows.Forms.TextBox txtDecryptedText;
+    }
+}
---
+++ lab3/lab3/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, char> reverseSubstitutionTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadTable_Click(object sender, EventArgs e)
+        {
+            // Вибір файлу для завантаження таблиці шифрування
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Завантаження таблиці шифрування з файлу
+                    reverseSubstitutionTable = LoadReverseSubstitutionTable(openFileDialog.FileName);
+                    MessageBox.Show("Таблиця шифрування завантажена.");
+                }
+            }
+        }
+
+        private void btnDecryptText_Click(object sender, EventArgs e)
+        {
+            if (reverseSubstitutionTable == null)
+            {
+                MessageBox.Show("Спочатку завантажте таблицю шифрування.");
+                return;
+            }
+
+            // Вибір файлу для зчитування зашифрованого тексту
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Зчитування зашифрованого тексту з файлу
+                    string encryptedText = File.ReadAllText(openFileDialog.FileName);
+                    string decryptedText = DecryptText(encryptedText, reverseSubstitutionTable);
+
+                    // Виведення розшифрованого тексту в текстове поле
+                    txtDecryptedText.Text = decryptedText;
+                    MessageBox.Show("Текст розшифровано.");
+                }
+            }
+        }
+
+        // Завантаження таблиці шифрування з файлу
+        private Dictionary<string, char> LoadReverseSubstitutionTable(string filePath)
+        {
+            var reverseTable = new Dictionary<string, char>();
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var parts = line.Split(':');
+                if (parts.Length == 2)
+                {
+                    char key = parts[0][0];
+                    var values = parts[1].Split(',');
+                    foreach (var value in values)
+                    {
+                        reverseTable[value] = key;
+                    }
+                }
+            }
+            return reverseTable;
+        }
+
+        // Розшифрування тексту з використанням таблиці заміни
+        private string DecryptText(string encryptedText, Dictionary<string, char> reverseTable)
+        {
+            var decryptedText = "";
+            var currentCode = "";
+
+            foreach (char c in encryptedText)
+            {
+                currentCode += c;
+                if (reverseTable.ContainsKey(currentCode))
+                {
+                    decryptedText += reverseTable[currentCode];
+                    currentCode = "";
+                }
+            }
+
+            return decryptedText;
+        }
+    }
+}

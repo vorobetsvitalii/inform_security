a1c13bda2ab00abf5d00903812d7e210879cd5dd
vorobetsvitalii <vorobetsvitaliy3@gmail.com>
2024-09-18 21:14:42 +0300

    lab5

---
+++ lab5/lab5/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnLoadKey;
+        private System.Windows.Forms.TextBox txtKeyPath;
+        private System.Windows.Forms.Button btnLoadInput;
+        private System.Windows.Forms.TextBox txtInputPath;
+        private System.Windows.Forms.Button btnSaveOutput;
+        private System.Windows.Forms.TextBox txtOutputPath;
+        private System.Windows.Forms.Label lblGamma;
+        private System.Windows.Forms.TextBox txtGamma;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label lblKey;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblOutput;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnLoadKey = new System.Windows.Forms.Button();
+            this.txtKeyPath = new System.Windows.Forms.TextBox();
+            this.btnLoadInput = new System.Windows.Forms.Button();
+            this.txtInputPath = new System.Windows.Forms.TextBox();
+            this.btnSaveOutput = new System.Windows.Forms.Button();
+            this.txtOutputPath = new System.Windows.Forms.TextBox();
+            this.lblGamma = new System.Windows.Forms.Label();
+            this.txtGamma = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLoadKey
+            // 
+            this.btnLoadKey.Location = new System.Drawing.Point(12, 30);
+            this.btnLoadKey.Name = "btnLoadKey";
+            this.btnLoadKey.Size = new System.Drawing.Size(120, 23);
+            this.btnLoadKey.TabIndex = 0;
+            this.btnLoadKey.Text = "Вибрати таблицю-ключ";
+            this.btnLoadKey.UseVisualStyleBackColor = true;
+            this.btnLoadKey.Click += new System.EventHandler(this.btnLoadKey_Click);
+            // 
+            // txtKeyPath
+            // 
+            this.txtKeyPath.Location = new System.Drawing.Point(150, 32);
+            this.txtKeyPath.Name = "txtKeyPath";
+            this.txtKeyPath.ReadOnly = true;
+            this.txtKeyPath.Size = new System.Drawing.Size(300, 20);
+            this.txtKeyPath.TabIndex = 1;
+            // 
+            // btnLoadInput
+            // 
+            this.btnLoadInput.Location = new System.Drawing.Point(12, 70);
+            this.btnLoadInput.Name = "btnLoadInput";
+            this.btnLoadInput.Size = new System.Drawing.Size(120, 23);
+            this.btnLoadInput.TabIndex = 2;
+            this.btnLoadInput.Text = "Вибрати вхідний файл";
+            this.btnLoadInput.UseVisualStyleBackColor = true;
+            this.btnLoadInput.Click += new System.EventHandler(this.btnLoadInput_Click);
+            // 
+            // txtInputPath
+            // 
+            this.txtInputPath.Location = new System.Drawing.Point(150, 72);
+            this.txtInputPath.Name = "txtInputPath";
+            this.txtInputPath.ReadOnly = true;
+            this.txtInputPath.Size = new System.Drawing.Size(300, 20);
+            this.txtInputPath.TabIndex = 3;
+            // 
+            // btnSaveOutput
+            // 
+            this.btnSaveOutput.Location = new System.Drawing.Point(12, 110);
+            this.btnSaveOutput.Name = "btnSaveOutput";
+            this.btnSaveOutput.Size = new System.Drawing.Size(120, 23);
+            this.btnSaveOutput.TabIndex = 4;
+            this.btnSaveOutput.Text = "Вибрати вихідний файл";
+            this.btnSaveOutput.UseVisualStyleBackColor = true;
+            this.btnSaveOutput.Click += new System.EventHandler(this.btnSaveOutput_Click);
+            // 
+            // txtOutputPath
+            // 
+            this.txtOutputPath.Location = new System.Drawing.Point(150, 112);
+            this.txtOutputPath.Name = "txtOutputPath";
+            this.txtOutputPath.ReadOnly = true;
+            this.txtOutputPath.Size = new System.Drawing.Size(300, 20);
+            this.txtOutputPath.TabIndex = 5;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(150, 190);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(120, 30);
+            this.btnEncrypt.TabIndex = 8;
+            this.btnEncrypt.Text = "Шифрувати";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(330, 190);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(120, 30);
+            this.btnDecrypt.TabIndex = 9;
+            this.btnDecrypt.Text = "Дешифрувати";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // lblKey
+            // 
+            this.lblKey.AutoSize = true;
+            this.lblKey.Location = new System.Drawing.Point(150, 15);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(105, 13);
+            this.lblKey.TabIndex = 10;
+            this.lblKey.Text = "Шлях до таблиці-ключа";
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(150, 55);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(87, 13);
+            this.lblInput.TabIndex = 11;
+            this.lblInput.Text = "Шлях до вхідного файлу";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(150, 95);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(93, 13);
+            this.lblOutput.TabIndex = 12;
+            this.lblOutput.Text = "Шлях до вихідного файлу";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(480, 240);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblKey);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtOutputPath);
+            this.Controls.Add(this.btnSaveOutput);
+            this.Controls.Add(this.txtInputPath);
+            this.Controls.Add(this.btnLoadInput);
+            this.Controls.Add(this.txtKeyPath);
+            this.Controls.Add(this.btnLoadKey);
+            this.Name = "Form1";
+            this.Text = "Шифр Плейфера";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ lab5/lab5/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        private int length;
+        private char[,] playfairTable;
+        private Dictionary<char, Tuple<int, int>> charPositions = new Dictionary<char, Tuple<int, int>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadKey_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Виберіть файл таблиці-ключа";
+            ofd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtKeyPath.Text = ofd.FileName;
+                LoadPlayfairKey(ofd.FileName);
+            }
+        }
+
+        private void LoadPlayfairKey(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                length = lines.Length;
+                playfairTable = new char[length, length];
+
+                for (int i = 0; i < length; i++)
+                {
+                    string line = lines[i].Replace(" ", "").Replace(",", "").Replace("\t", "");
+                    if (line.Length != length)
+                    {
+                        MessageBox.Show($"Рядок {i + 1} таблиці-ключа повинен містити {length} символів.");
+                        return;
+                    }
+
+                    for (int j = 0; j < length; j++)
+                    {
+                        playfairTable[i, j] = line[j];
+                        if (!charPositions.ContainsKey(line[j]))
+                        {
+                            charPositions.Add(line[j], Tuple.Create(i, j));
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Літера '{line[j]}' повторюється у таблиці-ключі.");
+                            return;
+                        }
+                    }
+                }
+
+                MessageBox.Show("Таблиця-ключ успішно завантажена.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при завантаженні таблиці-ключа: {ex.Message}");
+            }
+        }
+
+        private void btnLoadInput_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Виберіть вхідний файл";
+            ofd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtInputPath.Text = ofd.FileName;
+            }
+        }
+
+        private void btnSaveOutput_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Виберіть вихідний файл";
+            sfd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                txtOutputPath.Text = sfd.FileName;
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInputs())
+                return;
+
+            string openText = File.ReadAllText(txtInputPath.Text);
+            string processedText = PrepareText(openText);
+            List<string> bigrams = CreateBigrams(processedText);
+            string encryptedText = "";
+
+            foreach (var bigram in bigrams)
+            {
+                encryptedText += EncryptBigram(bigram);
+            }
+
+            File.WriteAllText(txtOutputPath.Text, encryptedText);
+            MessageBox.Show("Шифрування завершено.");
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInputs())
+                return;
+
+            string encryptedText = File.ReadAllText(txtInputPath.Text);
+            List<string> bigrams = CreateBigrams(encryptedText);
+            string decryptedText = "";
+
+            foreach (var bigram in bigrams)
+            {
+                decryptedText += DecryptBigram(bigram);
+            }
+
+            File.WriteAllText(txtOutputPath.Text, decryptedText);
+            MessageBox.Show("Дешифрування завершено.");
+        }
+
+        private bool ValidateInputs()
+        {
+            if (string.IsNullOrEmpty(txtKeyPath.Text))
+            {
+                MessageBox.Show("Будь ласка, виберіть файл таблиці-ключа.");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtInputPath.Text))
+            {
+                MessageBox.Show("Будь ласка, виберіть вхідний файл.");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtOutputPath.Text))
+            {
+                MessageBox.Show("Будь ласка, виберіть вихідний файл.");
+                return false;
+            }
+
+            if (playfairTable.Cast<char>().All(c => c == '\0'))
+            {
+                MessageBox.Show("Будь ласка, завантажте таблицю-ключ.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private string PrepareText(string text)
+        {
+            // Видаляємо всі символи, крім літер
+            var letters = text.Where(c => char.IsLetter(c)).ToList();
+
+            // Замінюємо 'J' на 'I' (для англійської, адаптуємо для української)
+            for (int i = 0; i < letters.Count; i++)
+            {
+                if (letters[i] == 'Й')
+                    letters[i] = 'Ї';
+                else if (letters[i] == 'й')
+                    letters[i] = 'ї';
+            }
+
+            // Якщо кількість літер непарна, додаємо 'X' (або інший символ)
+            if (letters.Count % 2 != 0)
+                letters.Add('X');
+
+            return new string(letters.ToArray());
+        }
+
+        private List<string> CreateBigrams(string text)
+        {
+            List<string> bigrams = new List<string>();
+
+            for (int i = 0; i < text.Length; i += 2)
+            {
+                string bigram = text.Substring(i, 2);
+
+                // Якщо літери однакові, замінюємо другу на 'X'
+                if (bigram[0] == bigram[1])
+                {
+                    bigram = bigram[0] + "X";
+                    i--; // Повторюємо цей крок з новим символом
+                }
+
+                bigrams.Add(bigram);
+            }
+
+            return bigrams;
+        }
+
+        private string EncryptBigram(string bigram)
+        {
+            char a = bigram[0];
+            char b = bigram[1];
+
+            try
+            {
+
+                Tuple<int, int> posA = charPositions[a];
+                Tuple<int, int> posB = charPositions[b];
+
+                if (posA.Item1 == posB.Item1)
+                {
+                    // Правило 2: однаковий рядок
+                    int colA = (posA.Item2 + 1) % length;
+                    int colB = (posB.Item2 + 1) % length;
+                    return playfairTable[posA.Item1, colA].ToString() + playfairTable[posB.Item1, colB].ToString();
+                }
+                else if (posA.Item2 == posB.Item2)
+                {
+                    // Правило 3: однаковий стовпець
+                    int rowA = (posA.Item1 + 1) % length;
+                    int rowB = (posB.Item1 + 1) % length;
+                    return playfairTable[rowA, posA.Item2].ToString() + playfairTable[rowB, posB.Item2].ToString();
+                }
+                else
+                {
+                    // Правило 1: утворюють прямокутник
+                    return playfairTable[posA.Item1, posB.Item2].ToString() + playfairTable[posB.Item1, posA.Item2].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при обробці тексту: {ex.Message}");
+                return "";
+            }
+        }
+
+        private string DecryptBigram(string bigram)
+        {
+            char a = bigram[0];
+            char b = bigram[1];
+
+            Tuple<int, int> posA = charPositions[a];
+            Tuple<int, int> posB = charPositions[b];
+
+            if (posA.Item1 == posB.Item1)
+            {
+                // Правило 2: однаковий рядок
+                int colA = (posA.Item2 - 1 + length) % length;
+                int colB = (posB.Item2 - 1 + length) % length;
+                return playfairTable[posA.Item1, colA].ToString() + playfairTable[posB.Item1, colB].ToString();
+            }
+            else if (posA.Item2 == posB.Item2)
+            {
+                // Правило 3: однаковий стовпець
+                int rowA = (posA.Item1 - 1 + length) % length;
+                int rowB = (posB.Item1 - 1 + length) % length;
+                return playfairTable[rowA, posA.Item2].ToString() + playfairTable[rowB, posB.Item2].ToString();
+            }
+            else
+            {
+                // Правило 1: утворюють прямокутник
+                return playfairTable[posA.Item1, posB.Item2].ToString() + playfairTable[posB.Item1, posA.Item2].ToString();
+            }
+        }
+    }
+}
